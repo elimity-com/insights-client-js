2887a01d24ad6470a3121c4927b79448781be7fb
jasper-elimity <jasper@elimity.com>
2024-10-04 14:40:04 +0200

    Add extended example script

---
+++ example/index.js
@@ -0,0 +1,9 @@
+import fixture from "./fixture.json";
+import { performImport } from "@elimity/insights-client";
+
+const config = {
+  baseUrl: "https://example.elimity.com/api",
+  sourceId: 1,
+  sourceToken: "my-token",
+};
+await performImport(config, fixture.entities, fixture.relationships);
